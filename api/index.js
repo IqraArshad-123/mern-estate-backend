b76c43f87d0e2c071e4b3128942f2157aab7555a
IqraArshad-123 <iqraa6244@gmail.com>
2026-01-04 23:38:06 +0500

    Fixing frontend

--- api/index.js
+++ api/index.js
@@ -1,59 +1,3 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import userRouter from './routes/user.route.js';
-// import authRouter from './routes/auth.route.js';
-// import listingRouter from './routes/listing.route.js';
-// import cookieParser from 'cookie-parser';
-// import uploadRoute from './routes/upload.route.js';
-// import cors from 'cors';
-
-// dotenv.config();
-
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log('Connected to MongoDB!'))
-//   .catch(err => console.log(err));
-
-// const app = express();
-
-// // Middlewares
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // 🔹 CORS configuration for local frontend
-
-// app.use(cors({
-//   origin: [
-//     'http://localhost:5173', // local dev
-//     // 'https://mern-estate-frontend-nine.vercel.app', // deployed frontend
-//     'https://mern-estate-front-end.netlify.app'
-  
-//   ],
-//   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-//   credentials: true
-// }));
-
-
-
-// // ROUTES
-// app.use('/api/upload', uploadRoute);
-// app.use('/api/user', userRouter);
-// app.use('/api/auth', authRouter);
-// app.use('/api/listing', listingRouter);
-
-// // ERROR HANDLER
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || 'Internal Server Error';
-//   return res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
-
-// export default app;
-
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -70,7 +14,7 @@ const app = express();
 
 /* 🔹 CORS – ALWAYS FIRST */
 app.use(cors({
-  origin: true,
+  origin: 'https://mern-estate-frontend-nine.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
